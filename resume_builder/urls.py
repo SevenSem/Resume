5623fb05e18a7edd84ff8005ce34b92524b38c6a
Anusha Panta <anusha474232@gmail.com>
2020-10-23 09:11:21 +0545

    image work completed.

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('resume.urls')),
     path('account/', include('account.urls')),
     path('personality/', include('personality.urls')),
-]+static(settings.MEDIA_URL,ødocument_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
