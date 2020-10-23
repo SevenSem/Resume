d4af0cdda6b2e595d76ff15a7a50f4cd7c06df09
achyutneupane <achyut1324@outlook.com>
2020-10-23 09:05:26 +0545

    add chart

--- resume/urls.py
+++ resume/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path , include
 from . import views
+
 urlpatterns = [
+    
     path('', views.index, name = 'home'),
     path('createresume', views.resumeForm, name='resumeform'),
     path('choosetemplates',views.choosetemplates,name='choosetemplates'),
@@ -8,5 +10,7 @@ urlpatterns = [
     path('resume_Update/<id>',views.resume_Update,name='resume_Update'),
     path('templates/<id>',views.templates,name='templates'),
     path('pdf_view/<id>', views.ViewPDF.as_view(), name="pdf_view"),
+    path('chart', views.chart, name='chart'),
     path('prediction/',include('prediction.urls'))
+
     ]
