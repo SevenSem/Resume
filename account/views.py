27859f2c64f4de6949695de53281afa6b5c23224
achyutneupane <achyut1324@outlook.com>
2021-05-03 15:19:59 +0545

    email confirmation

--- account/views.py
+++ account/views.py
@@ -8,6 +8,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.conf import settings
 
+from django.core.mail import send_mail  
+from django.template.loader import get_template
+from django.core.mail import EmailMultiAlternatives
+from django.template import Context
 
 User = settings.AUTH_USER_MODEL
 # Create your views here.
@@ -17,6 +21,19 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+
+            ######################### mail system ####################################
+            htmly = get_template('pages/email.html')
+            d = { 'username': username }
+            subject, from_email, to = 'Welcome', 'a4collegeteam@gmail.com', email
+            html_content = htmly.render(d)
+            msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+            ##################################################################
+            messages.success(request, f'Your account has been created ! You are now able to log in')
             return redirect('login')
 
     else:
